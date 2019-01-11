9406dbb6a8904a09c2e4afae5e5b6cc300fac362
TonyWhiteb <tonybai2016@gmail.com>
2019-01-11 15:31:02 -0600

    Update1112019

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import os 
 import itertools
-
+def chunks(iterable, n):
+    iterable = iter(iterable)
+    num = len(n)
+    
+    while True:
+        yield chain([next(iterable)],islice(iterable, ))
 with open('XSS1-EOS-2018FA.TXT') as f:
     # for line in itertools.islice(f, 4, 60):
     #     print(line)
-    print(type(itertools.islice(f, 4, 60)))
+    # print(type(itertools.islice(f, 4, 60)))
+    for i, lines in enumerate(chunks(f,4,60))
