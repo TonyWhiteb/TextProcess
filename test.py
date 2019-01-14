8b74829e019ed22ab6d81c1d2298a9d41f79c671
TonyWhiteb <tonybai2016@gmail.com>
2019-01-14 16:16:12 -0600

    Update0114

--- test.py
+++ test.py
@@ -1,13 +1,39 @@
 import os 
-import itertools
+from itertools import chain, islice
+# def chunks(iterable, n):
+
+#     iterable = iter(iterable)
+    
+#     while True:
+#         yield chain([next(iterable)],islice(iterable, ))
+
 def chunks(iterable, n):
     iterable = iter(iterable)
-    num = len(n)
-    
     while True:
-        yield chain([next(iterable)],islice(iterable, ))
+        delimiter = iter(n)
+        for i in delimiter:
+            yield  chain([next(iterable)],islice(iterable,i,next(delimiter)))
+
+delimiter = [4, 60, 116, 177]
 with open('XSS1-EOS-2018FA.TXT') as f:
     # for line in itertools.islice(f, 4, 60):
     #     print(line)
     # print(type(itertools.islice(f, 4, 60)))
-    for i, lines in enumerate(chunks(f,4,60))
+    # item = iter(delimiter)
+    # for x in item:
+    #     n = x
+    #     m = next(item)
+    #     for a in islice(f,n,m):
+    #         print(a)
+    # for a in islice(f,item,next(item)):
+    #     print(a)
+    for i, lines in enumerate(chunks(f,delimiter)):
+        file_split = '{}.{}'.format(f,i)
+        with open(file_split,'w') as fi:
+            fi.writelines(lines)
+    # for i in enumerate(chunks(f,delimiter)):
+    #     print(i)
+
+
+
+
---
+++ test2.py
@@ -0,0 +1,6 @@
+seq = [1,2,3,4,5,6,7,8,9,10]
+it = iter(seq)
+for x in it:
+    n = x
+    m = next(x)
+    print (n, m)
